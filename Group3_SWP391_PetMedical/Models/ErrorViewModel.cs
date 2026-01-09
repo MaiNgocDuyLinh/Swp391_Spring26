2b7feeb6bf4260975a9abe06b8cb985a31232f4e
linhmaideptrai <duylinh2992004@gmail.com>
2026-01-10 01:54:29 +0700

    Initial project structure

---
+++ Group3_SWP391_PetMedical/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group3_SWP391_PetMedical.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
